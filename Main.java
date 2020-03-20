ed49de62e715d2f07ede27633eb9367fdcb79bd6
HaVanVu1708 <62437220+HaVanVu1708@users.noreply.github.com>
2020-03-20 23:47:43 +0700

    Java_02

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world!");
+        System.out.println("Hello world again!");
+        System.out.println();
+        System.out.println("Hello world!");
+        System.out.println("Hello world again!");
+        System.out.println();
+        System.out.println("Hello,");
+        System.out.println(" ");
+        System.out.println("world!");
+        System.out.println("Hello,world!");
+
+
+        //        hinh a
+        System.out.println("* * * * *\n * * * * *\n* * * * *\n * * * * *\n* * * * *\n");
+//      hinh b
+        System.out.println("* * * * * \n*       *\n*       *\n*       *\n* * * * * \n");
+//      hinh c
+        System.out.println("* * * * * *\n *       *\n  *     *\n   *   *\n     *\n");
+//      hinh D
+        System.out.println("    *\n * *  * *\n   * *\n  *  *\n *    *\n");
+//
+
+    }
+}
